3bfc2115704f7854fff6c5b1b22c06a0c5ca5885
4NG <63141159+4NG@users.noreply.github.com>
2020-04-14 22:01:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 from Preprocessing_data import *
-from Understand_data import *
 
-# optionString = input("Type Option Here: ")
-inputFileString = input("Type Input File Name Here: ")
-outputFileString = input("Output File Name Here: ")
-logFileString = input("Output File Name Here: ")
+print("                     Welcome!!!                 \n")
+print("      Nhom 17 - LAB1 - Preprocessing Data       \n")
+print("First, please set up some step: \n")
 
-# while True:
-#     try:
-#         input_data = pd.read_csv(inputFileString)
-#         break
-#     except:
-#         print("There is no file name.")
-#         data = input("Try again! Input your data here: ")
+inputFileString = input("1. Input your file Name Here. This must be a .csv file. For example: test, hello,...  :")
+outputFileString = input("2. Name your Output file Name Here. This is a .csv file. For example: output, out,...  :")
+logFileString = input("2. Name your Log file Name Here. This is a .txt file. For example: log, log_file,...  :")
 
-# Understand data
-# understand = UnderstandData(data)
-# df_columns = input_data.columns.tolist()
-# for i in df_columns:
-#     if input_data[i].dtypes == object:
-#         print(understand.describe_nominal(input_data[i]))
-#     else:
-#         print(understand.describe_numeric(input_data[i]))
 def formatParamString(inputFileString, outputFileString, logFileString):
     inputFileString = inputFileString + '.csv'
     outputFileString = outputFileString + '.csv'
@@ -32,5 +18,41 @@ def formatParamString(inputFileString, outputFileString, logFileString):
 
 inputFileString, outputFileString, logFileString = formatParamString(inputFileString, outputFileString, logFileString)
 
-discretize = Preprocessing_data(inputFileString, outputFileString, logFileString)
-discretize.discretize()
+while True:
+    data = ProcessingData(inputFileString, outputFileString, logFileString)
+    choice = input("Choose your options: \n"
+          "1. Summary \n"
+          "2. Replace \n"
+          "3. Discretize \n"
+          "4. Normalize \n"
+          "5. Exit \n")
+
+    if choice == '1':
+       data.summary(inputFileString)
+       print("This will be wrote down in your {}.".format(logFileString))
+       print("Check out {} file later!!!".format(logFileString))
+       print('\n')
+
+    elif choice == '2':
+        data.replace(inputFileString)
+        print("This will be wrote down in your {} and {}.".format(outputFileString, logFileString))
+        print("Check out {} and {} files later!!!".format(outputFileString, logFileString))
+        print('\n')
+
+    elif choice == '3':
+        data.discretize(inputFileString)
+        print("This will be wrote down in your {} and {}.".format(outputFileString, logFileString))
+        print("Check out {} and {} files later!!!".format(outputFileString, logFileString))
+        print('\n')
+
+    elif choice == '4':
+        data.normalize(inputFileString)
+        print("This will be wrote down in your {} and {}.".format(outputFileString, logFileString))
+        print("Check out {} and {} files later!!!".format(outputFileString, logFileString))
+        print('\n')
+
+    elif choice == '5':
+        print("Thanks for using our program!!! \n")
+        print("Remember to check out files. \n")
+        print("Good bye!!!")
+        break
